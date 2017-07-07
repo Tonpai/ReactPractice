432f109f8e7b4a6107fe7fdd186bceff5343464a
Tonpai <itonpai.bkk@gmail.com>
2017-07-07 15:31:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Bootstrap from './BootstrapImport.js'
+import $ from 'jquery';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Bootstrap.container">
+      ทดสอบ
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/BootstrapImport.js
@@ -0,0 +1,4 @@
+
+import './bootstrap-4.0.0-alpha.6-dist/css/bootstrap.min.css'
+import './bootstrap-4.0.0-alpha.6-dist/js/bootstrap.min.js'
+import jQuery from 'jquery'
